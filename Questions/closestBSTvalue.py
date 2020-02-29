efa9c360e5e77c52e3f36d52deb0b3d9948698e6
ayush-algoexpert <ayushsmash@gmail.com>
2020-02-29 22:25:46 +0530

    Day 2

--- Questions/closestBSTvalue.py
+++ Questions/closestBSTvalue.py
@@ -1,5 +1,19 @@
-# a method x(depth,base):
-# if base.right=null and depth!=0
-# call x(depth-1,)
-#else if base.left!=null
-# call x(depth+1, base.left)
+def findClosestValueInBst(tree, target):
+    # Write your code here.
+	closestMate = tree.value
+	print("target: " + str(target))
+	closestMate = traverseBST(tree, target, closestMate)
+	return closestMate
+	
+
+def traverseBST(tree, target, closestMate):
+	# base case
+	if abs(tree.value - target) <= abs(closestMate-target):
+		closestMate = tree.value;
+	
+	#recursion	
+	if(tree.left != None):
+		closestMate = traverseBST(tree.left, target, closestMate)
+	if(tree.right != None):
+		closestMate = traverseBST(tree.right, target, closestMate)
+	return closestMate
